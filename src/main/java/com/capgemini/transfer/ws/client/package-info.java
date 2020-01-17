c0bc809e7dad3e844e9fbeee4c50bb8751f80663
L5odan <42061507+L5odan@users.noreply.github.com>
2020-01-17 15:16:41 +0800

    Dummy

--- src/main/java/com/capgemini/transfer/ws/client/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ATS.online-onboarding.com/Client/HRDataServiceEx", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.capgemini.transfer.ws.client;
